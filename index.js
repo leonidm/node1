ad4dc93ddc26c9ca4a05faee7a155debda1f1ada
leomrg861 <leomrg861@gmail.com>
2018-11-08 14:33:49 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer();
+
+server.on('request', (req, res) => {
+    res.write('Hello from my server...');
+    res.end();
+})
+
+server.listen(8000, () => console.log('Server started...'));
